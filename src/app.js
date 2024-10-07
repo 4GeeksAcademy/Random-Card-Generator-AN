9b46124303704253b647df3803cac2a16025e7fc
Sajadev404 <empresasarn@gmail.com>
2024-10-07 20:17:55 +0000

    firts commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "bootstrap";
+const card = document.querySelector(".card");
+const height = document.querySelector("#height");
+const width = document.querySelector("#width");
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "K",
+    "Q",
+    "J",
+    "10",
+    "9",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let RandomN = Math.floor(Math.random() * numbers.length);
+  return numbers[RandomN];
+};
+let gerenatecardsuit = () => {
+  let cardsuit = ["diamond", "spade", "heart", "club"];
+  let randomsuit = Math.floor(Math.random() * cardsuit.length);
+  return cardsuit[randomsuit];
+};
+let updatecardsize = () => {
+  let x = width.value ? `${width.value}px` : "";
+  card.style.width = x;
+  let y = height.value ? `${height.value}px` : "";
+  card.style.height = y;
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(gerenatecardsuit());
+  document.querySelector(".card").innerHTML = generateRandomNumber();
+  height.addEventListener("input", updatecardsize);
+  width.addEventListener("input", updatecardsize);
 };
