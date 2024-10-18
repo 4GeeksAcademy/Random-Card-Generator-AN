6e5efd32b7ccb2741a9bb2a4df8b95d83f9ad482
Sajadev404 <empresasarn@gmail.com>
2024-10-18 13:05:46 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import "./style.css";
 import "bootstrap";
-const card = document.querySelector(".card");
-const height = document.querySelector("#height");
-const width = document.querySelector("#width");
-let generateRandomNumber = () => {
+
+window.onload = function() {
   let numbers = [
     "A",
     "K",
@@ -19,25 +17,103 @@ let generateRandomNumber = () => {
     "3",
     "2"
   ];
+  let cardsuit = ["♠", "♣", "♥", "♦"];
 
-  let RandomN = Math.floor(Math.random() * numbers.length);
-  return numbers[RandomN];
-};
-let gerenatecardsuit = () => {
-  let cardsuit = ["diamond", "spade", "heart", "club"];
-  let randomsuit = Math.floor(Math.random() * cardsuit.length);
-  return cardsuit[randomsuit];
-};
-let updatecardsize = () => {
-  let x = width.value ? `${width.value}px` : "";
-  card.style.width = x;
-  let y = height.value ? `${height.value}px` : "";
-  card.style.height = y;
-};
+  /**@generatecartsuit funcion que devuelve numero y dibujo aleatorio de una carta
+   *@variables
+   * variables utilizadas
+   *topsuit
+   *numbers
+   *bottomsuit
+   */
+  let gerenatecardsuit = () => {
+    let RandomN = numbers[Math.floor(Math.random() * numbers.length)];
+    document.getElementById("number").innerHTML = RandomN;
 
-window.onload = () => {
-  document.querySelector(".card").classList.add(gerenatecardsuit());
-  document.querySelector(".card").innerHTML = generateRandomNumber();
-  height.addEventListener("input", updatecardsize);
-  width.addEventListener("input", updatecardsize);
+    let randomsuit = cardsuit[Math.floor(Math.random() * cardsuit.length)];
+    let topsuit = document.getElementById("top-suit");
+    let bottomsuit = document.getElementById("bottom-suit");
+    topsuit.innerHTML = randomsuit;
+    bottomsuit.innerHTML = randomsuit;
+    //se utiliza para colorear en rojo las cartas de la baraja corazon y diamante
+    if (randomsuit == "♥" || randomsuit == "♦") {
+      topsuit.classList.add("red-suit");
+      bottomsuit.classList.add("red-suit");
+    } else {
+      topsuit.classList.remove("red-suit");
+      bottomsuit.classList.remove("red-suit");
+    }
+  };
+
+  //funcion/evento listener al click del raton para generar la carta
+
+  document.getElementById("newcard").addEventListener("click", () => {
+    gerenatecardsuit();
+  });
+  //funcion para generar una carta cada 10s
+  setInterval(gerenatecardsuit, 10 * 1000);
+  gerenatecardsuit();
+
+  //funcion para ajustar la carta deseada por el usuario
+  //para evitar problemas ajustaremos un minimo y maximo
+  function cSizeCard() {
+    let userWidth = parseInt(document.getElementById("width").value, 10);
+    let userHeight = parseInt(document.getElementById("height").value, 10);
+
+   
+    const minWidth = 100;
+    const maxWidth = 500;
+    const minHeight = 140;
+    const maxHeight = 700;
+
+    
+
+    if (userWidth < minWidth) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: "El ancho mínimo es" + `${minWidth}px.`
+      });
+      userWidth = minWidth;
+    } else if (userWidth > maxWidth) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: "El ancho mínimo es " + `${maxWidth}px.`
+      });
+      userWidth = maxWidth;
+    }
+
+    
+
+    if (userHeight < minHeight) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: "El alto mínimo es" + `${minHeight}px.`
+      });
+      userHeight = minHeight;
+    } else if (userHeight > maxHeight) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: "El alto mínimo es " + `${maxHeight}px.`
+      });
+      userHeight = maxHeight;
+    }
+
+   
+
+    let myDiv = document.getElementById("card");
+    myDiv.style.width = `${userWidth}px`;
+    myDiv.style.height = `${userHeight}px`;
+  }
+
+  document.getElementById("chsize").addEventListener("click", function() {
+    cSizeCard();
+  });
 };
